6d37cb6d7d85cbe5d2f0808941485abea72aca27
Brian Maggi <brianmaggi@gmail.com>
2025-06-09 17:35:53 -0700

    Add Article and PageClient components; implement article fetching and rendering

--- src/app/(frontend)/articles/page.tsx
+++ src/app/(frontend)/articles/page.tsx
@@ -1,3 +1,55 @@
+import type { Metadata } from 'next/types'
+
+import { CollectionArchive } from '@/components/CollectionArchive'
+import { ArticleArchive } from '@/components/ArticleArchive'
+import { PageRange } from '@/components/PageRange'
+import { Pagination } from '@/components/Pagination'
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
+import React from 'react'
+import PageClient from './page.client'
+
+export const dynamic = 'force-static'
+export const revalidate = 600
+
 export default async function Page() {
-  return <div>Articles</div>
+  const payload = await getPayload({ config: configPromise })
+
+  const articles = await payload.find({
+    collection: 'articles',
+    depth: 1,
+    limit: 12,
+    overrideAccess: false,
+    select: {
+      title: true,
+      slug: true,
+      categories: true,
+      meta: true,
+    },
+  })
+
+  return (
+    <div className="pt-24 pb-24">
+      <PageClient />
+      <div className="container mb-16">
+        <div className="prose dark:prose-invert max-w-none">
+          <h1>Articles</h1>
+        </div>
+      </div>
+
+      <ArticleArchive articles={articles.docs} />
+
+      <div className="container">
+        {articles.totalPages > 1 && articles.page && (
+          <Pagination page={articles.page} totalPages={articles.totalPages} />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export function generateMetadata(): Metadata {
+  return {
+    title: `Payload Website Template Posts`,
+  }
 }
---
+++ src/components/ArticleArchive/index.tsx
@@ -0,0 +1,32 @@
+import { cn } from '@/utilities/ui'
+import React from 'react'
+
+import { Card, CardPostData } from '@/components/Card'
+import { stringify } from 'querystring'
+
+export type Props = {
+  articles: CardPostData[]
+}
+
+export const ArticleArchive: React.FC<Props> = (props) => {
+  const { articles } = props
+
+  return (
+    <div className={cn('container')}>
+      <div>
+        <div className="">
+          {articles?.map((result, index) => {
+            if (typeof result === 'object' && result !== null) {
+              return (
+                <div className="col-span-4 prose" key={index}>
+                  <h3>{result.title}</h3>
+                </div>
+              )
+            }
+            return null
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
